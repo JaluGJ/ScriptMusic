d9377fdd92ce8d2816a85bb5d853d0d65b361c4d
Facundo Ortiz <facundoagustinortiz@gmail.com>
2022-06-29 13:32:35 -0300

    first commit

---
+++ final-project/App.js
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <Text>PROYECTO FINAL RE COPADO</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
