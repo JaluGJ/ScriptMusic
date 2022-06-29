417e88534f2f0d9bfd8547371b7991448d321b68
Facundo Ortiz <facundoagustinortiz@gmail.com>
2022-06-29 14:00:55 -0300

    commit de prueba

--- final-project/App.js
+++ final-project/App.js
@@ -4,7 +4,7 @@ import { Text, View } from 'react-native';
 export default function App() {
   return (
     <View>
-      <Text>PROYECTO FINAL RE COPADO</Text>
+      <Text>PROYECTO FINAL</Text>
       <StatusBar style="auto" />
     </View>
   );
