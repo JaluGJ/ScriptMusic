600a705f9dd73157e9d0a6a998e7e2a0df611319
JaluGJ <gabriel.i.jalil@gmail.com>
2022-07-05 12:41:25 -0300

    feat/show line to ivan

--- api/mongo.js
+++ api/mongo.js
@@ -4,6 +4,7 @@ const {DB_PASS} = process.env
 
 const connectionString = `mongodb+srv://jalugj:${DB_PASS}@cluster0.ncpug.mongodb.net/?retryWrites=true&w=majority`
 
+//prueba muestra a ivan
 //conneccion con mongoDB
 
 mongoose.connect(connectionString)
