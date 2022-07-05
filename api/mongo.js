d528a236e3c26da7eea2793a40e791cba06103ca
Broflotsky <82426082+Broflotsky@users.noreply.github.com>
2022-07-05 10:50:21 -0500

    Revert "feat/show line to ivan"

--- api/mongo.js
+++ api/mongo.js
@@ -4,7 +4,6 @@ const {DB_PASS} = process.env
 
 const connectionString = `mongodb+srv://jalugj:${DB_PASS}@cluster0.ncpug.mongodb.net/?retryWrites=true&w=majority`
 
-//prueba muestra a ivan
 //conneccion con mongoDB
 
 mongoose.connect(connectionString)
