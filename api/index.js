cb3470851982c33f237f98cde25ffa0d771a4c51
EnzoDosSantos <101943986+EnzoDosSantos@users.noreply.github.com>
2022-07-05 14:30:35 -0300

    feat/add GET POST

--- api/index.js
+++ api/index.js
@@ -13,8 +13,42 @@ const Product = require('./src/models/product/productSchema.js')
 app.use(cors())
 app.use(express.json())
 
-app.get('/products', (req, res, next) => {
+app.post('/products', (req, res, next) => {
+    const { model, brand, price, type, category, stock, image, description } = req.body
 
+    if(!model || !brand || !price || !type || !category || !stock || !image || !description) {
+        res.status(400).send('Missing data')
+    }
+
+    const product = new Product({
+        model,
+        brand,
+        price,
+        type,
+        category,
+        stock,
+        image,
+        description
+    })
+
+    product.save()
+        .then(() => {
+            res.send('Producto guardado')
+        }
+        ).catch((err) => {
+            res.send(err)
+        })
+})
+
+app.get('/products', (req, res, next) => {
+    Product.find()
+        .then((products) => {
+            res.send(products)
+        }
+        ).catch((err) => {
+            res.send(err)
+        }
+    )
 })
 
 app.listen(PORT, () => {
