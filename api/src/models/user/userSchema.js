8f5edfadcf1fd5160590e0ea42d577f9ab6a7e4c
Gusberly <gusberlymorillo@gmail.com>
2022-07-06 12:57:22 -0300

    add UserSchema

--- api/src/models/user/userSchema.js
+++ api/src/models/user/userSchema.js
@@ -1,1 +1,31 @@
+const { Schema, model } = require("mongoose");
 
+const userSchema = new Schema({
+  firsName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+userSchema.set("toJSON", {
+  transform: (doc, ret) => {
+    delete ret.password;
+    return ret;
+  },
+});
+
+const userModel = model("user", userSchema);
+
+module.exports = userModel;
