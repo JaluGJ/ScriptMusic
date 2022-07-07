6bd929d98464404df8acfd441d405faa6acc4123
EnzoDosSantos <101943986+EnzoDosSantos@users.noreply.github.com>
2022-07-07 17:23:42 -0300

    feat/add reduxBasicPeticions

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 )
