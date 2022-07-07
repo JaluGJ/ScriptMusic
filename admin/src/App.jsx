13ac500e430f0fe35b243470b537925c591fa159
Broflotsky <ivancho0@hotmail.com>
2022-07-07 17:38:37 -0500

    feat/ add form to create new products

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './components/Home/Home';
+import NewProduct from './components/Newproduct/NewProduct';
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Home />}/>
+          <Route path='/new' element={ <NewProduct />}/>
         </Routes>
       </BrowserRouter>
     </div>
