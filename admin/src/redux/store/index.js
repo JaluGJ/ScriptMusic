8e5751f524297769d90be80730ad7fa8a05b5aee
JaluGJ <gabriel.i.jalil@gmail.com>
2022-07-08 11:54:24 -0300

    feat/add clodinary connection

--- admin/src/redux/store/index.js
+++ admin/src/redux/store/index.js
@@ -2,7 +2,7 @@ import { applyMiddleware, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 
-import rootReducer from '../reducer';
+import {rootReducer} from '../reducer';
 
 const store = createStore(
     rootReducer,
--- api/src/controllers/payment.js
+++ api/src/controllers/payment.js
@@ -5,22 +5,22 @@ const { STRIPE } = process.env
 const stripe = require('stripe')(STRIPE)
 
 module.exports = {
-    paymentCard: async (req, res) => {
-        const { items } = req.body
-        try {
-            const paymentIntent = await stripe.paymentIntents.create({
-                amount: calculateOrderAmount(items),
-                currency: 'usd',
-                automatic_payment_methods: {
-                    enabled: true,
-                }
-            })
-            return res.send({
-                clientSecret: paymentIntent.client_secret,
-            })
-        } catch (error) {
-            console.log(error)
-            return res.status(400).send('Algo falló')
+  paymentCard: async (req, res) => {
+    const { items } = req.body
+    try {
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: calculateOrderAmount(items),
+        currency: 'usd',
+        automatic_payment_methods: {
+          enabled: true,
         }
+      })
+      return res.send({
+        clientSecret: paymentIntent.client_secret,
+      })
+    } catch (error) {
+      console.log(error)
+      return res.status(400).send('Algo falló')
     }
+  }
 }
