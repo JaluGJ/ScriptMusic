3c4a9e431d9c94d4f5f6cc71b10522b890cd6bfd
Facundo Ortiz <facundoagustinortiz@gmail.com>
2022-07-13 13:17:00 -0300

    feat/add register complete

--- client/src/redux/slices/signup.js
+++ client/src/redux/slices/signup.js
@@ -7,15 +7,19 @@ export const signupSlice = createSlice({
   name: "signup",
   initialState: {
     token: "",
+    err: false,
   },
   reducers: {
     setToken: (state, action) => {
       state.token = action.payload;
     },
+    setErr: (state, action) => {
+      state.err = action.payload;
+    },
   },
 });
 
-export const { setToken } = signupSlice.actions;
+export const { setToken, setErr } = signupSlice.actions;
 
 export default signupSlice.reducer;
 
@@ -23,10 +27,17 @@ export const postUser = (obj) => (dispatch) => {
   axios
     .post(`${apiUrl}signup`, obj)
     .then((res) => {
-      console.log("User created succesfully");
       dispatch(setToken(res.data.token));
+      dispatch(setErr(false));
+      console.log("User created succesfully");
     })
     .catch((e) => {
+      dispatch(setToken(""));
+      dispatch(setErr(true));
       console.log("User already exists");
     });
+};
+
+export const errFalse = (err) => (dispatch) => {
+  dispatch(setErr(!err));
 };
