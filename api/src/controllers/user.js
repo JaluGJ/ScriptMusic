8e3079345f687983e89c9e770aeef0a6105a38e1
EnzoDosSantos <101943986+EnzoDosSantos@users.noreply.github.com>
2022-07-13 17:09:55 -0300

    feat/add Login-register

--- api/src/controllers/user.js
+++ api/src/controllers/user.js
@@ -1,30 +1,51 @@
+require('dotenv').config()
+const { JWT_SECRET } = process.env
+const jwt = require('jsonwebtoken')
 const User = require('../models/user/userSchema.js')
 
 
 module.exports = {
     
-    // createUser : (req, res, next) => {
-    //     const { firstName, lastName, email, password } = req.body
+    registerUser : async (req, res, next) => {
+        let { email, password, firstName, lastName, isAdmin } = req.body
+        try {
+            const user = await User.findOne({ email })
+            if (user) {
+                return res.status(404).json({ message: 'El e-mail ya ha sido tomado' })
+            }
+            isAdmin === undefined ?
+            isAdmin = false :
+            isAdmin = true
+            const newUser = {
+                email,
+                password,
+                firstName,
+                lastName,
+                isAdmin
+            }
+            const userCreated = await User.create(newUser)
+            return res.status(200).json({ userCreated })
+        } catch (error) {
+            next(error)
+        }
+    },
 
-    //     if(!firstName || !lastName || !email || !password) {
-    //         return res.status(400).send('Falta informacion necesaria')
-    //     }
-
-    //     const newUser = {
-    //         firstName,
-    //         lastName,
-    //         email,
-    //         password
-    //     }
-
-    //     User.create(newUser)
-    //         .then((user) => {
-    //             return res.json(user)
-    //         }
-    //         ).catch((error) => {
-    //             next(error)
-    //         }
-    //     )},
+    loginUser : async (req, res, next) => {
+        const { email, password } = req.body
+        try {
+            const user = await User.findOne({ email })
+            let validate = user === null ?
+            false 
+            : await user.isValidPassword(password)
+            if(!validate){
+                return res.status(401).json({ message: 'La contraseña o el e-mail son incorrectos' })
+            }
+            const token = jwt.sign({ id: user._id }, JWT_SECRET)
+            return res.json({ token })
+        } catch (error) {
+            next(error)
+        }
+    },
 
     getAllUsers : (req, res, next) => {
         User.find()
--- api/src/models/user/userSchema.js
+++ api/src/models/user/userSchema.js
@@ -46,7 +46,7 @@ userSchema.set('toJSON', {
 })
 
 userSchema.methods.isValidPassword = async function (password) {
-  const isMatch = await bcrypt.compare(password, user.password)
+  const isMatch = await bcrypt.compare(password, this.password)
   return isMatch
 }
 
