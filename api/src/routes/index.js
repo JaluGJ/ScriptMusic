527231245a3b5d998c00071ca9ea1f69d8418bf5
EnzoDosSantos <101943986+EnzoDosSantos@users.noreply.github.com>
2022-07-14 14:29:47 -0300

    feat/add AdminLogin & fix Search products

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,7 +1,7 @@
 const { Router } = require('express')
 //importar los componentes donde tienen todas las rutas
 const { getAllProducts, getProductById, updateProduct, uploadProduct, deleteProduct } = require('../controllers/products')
-const { getAllUsers, registerUser, loginUser } = require('../controllers/user')
+const { getAllUsers, registerUser, loginUser, loginAdmin } = require('../controllers/user')
 const { paymentCard } = require('../controllers/payment')
 const routes = Router()
 
@@ -25,58 +25,7 @@ routes.post('/login', loginUser)
 
 routes.post('/signup', registerUser)
 
-
-// routes.post('/signup', passport.authenticate('signup', { session: false }),
-//  async(req, res, next) => {
-//     try {
-//         const token = jwt.sign({ id: req.user._id }, JWT_SECRET, { expiresIn: 86400 })
-//         res.json({
-//             token,
-//             user: req.user
-//         })   
-//     } catch (error) {
-//         next(error)
-//     }
-// })
-
-// routes.post('/login', passport.authenticate('login', { session: false }),
-//     async(err, user, next) => {
-//         try {
-//             if(err || !user){
-//                 const error = new Error('Usuario o contraseña invalidos')
-//                 next(error)
-//             }
-//             req.login(user, { session: false }, async(err) => {
-//                 if(err){
-//                     next(err)
-//                 }
-//                 console.log("aaaaaa")
-//                 const body = {_id, email, firstName, lastName}
-//                 const token = jwt.sign({ user: body }, JWT_SECRET)
-//                 res.json({
-//                     user,
-//                     token
-//                 })
-//             })
-//         } catch (error) {
-//             next(error)
-//         }
-// })
-
-
-// routes.get('/profile', passport.authenticate('jwt', { session: false }),
-//     async(req, res, next) => {
-//         try {
-//             const user = await User.findById(req.user._id)
-//             res.json({
-//                 user,
-//                 token: req.query.token
-//             })
-//         } catch (error) {
-//             next(error)
-//         }
-// })
-
+routes.post('/loginAdmin', loginAdmin)
 
 routes.get('/users', getAllUsers)
 // USER ROUTES
