296a1487ecf316374a478f922a04d3dfc2d845a6
Broflotsky <ivancho0@hotmail.com>
2022-07-14 22:11:48 -0500

    feat/ edit button in products table working

--- admin/src/components/tableproducts/List.jsx
+++ admin/src/components/tableproducts/List.jsx
@@ -1,14 +1,6 @@
 import "./List.scss";
 import AddBoxIcon from "@mui/icons-material/AddBox";
 import { Link } from "react-router-dom";
-// import Table from '@mui/material/Table';
-// import TableBody from '@mui/material/TableBody';
-// import TableCell from '@mui/material/TableCell';
-// import TableContainer from '@mui/material/TableContainer';
-// import TableHead from '@mui/material/TableHead';
-// import TableRow from '@mui/material/TableRow';
-// import Paper from '@mui/material/Paper';
-// import ModeEditIcon from '@mui/icons-material/ModeEdit';
 import { DataGrid } from '@mui/x-data-grid';
 
 
@@ -25,48 +17,11 @@ export default function List({ rows, columns, title }) {
                 </Link>
             </div>
             <div className="bottom">
-                {/* <TableContainer component={Paper}>
-                    <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                        <TableHead>
-                            <TableRow>
-                                {
-                                    columns.map((e) =>  {
-                                        return (
-                                            <TableCell>{ e }</TableCell>
-                                        )
-                                    }
-                                    )
-                                }
-                                <TableCell>Editar/Eliminar</TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {rows.map((row) => (
-                                <TableRow
-                                    key={row.id}
-                                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                                >
-                                    <TableCell component="th" scope="row">{row.model}</TableCell>
-                                    <TableCell >{row.brand}</TableCell>
-                                    <TableCell ><img src={row.image} alt="" width='35px' height='35px'/></TableCell>
-                                    <TableCell >{row.price}</TableCell>
-                                    <TableCell >{row.category}</TableCell>
-                                    <TableCell >{row.type}</TableCell>
-                                    <TableCell >{row.stock}</TableCell>
-                                    <TableCell > {row.id} </TableCell>
-                                    <TableCell > <ModeEditIcon/> </TableCell>
-
-
-                                </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                </TableContainer> */}
-                <div style={{ height: 800, width: '90%' }}>
+                <div style={{ height: 800, width: '85%' }}>
                     <DataGrid
                         rows={rows}
                         columns={columns}
-                        pageSize={12}
+                        pageSize={30}
                         rowsPerPageOptions={[5]}                      
                     />
                 </div>
