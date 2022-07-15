c4256b884710bfbe250403686b6556d7f9a0d094
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-07-15 18:07:42 -0300

    homePrivate

--- admin/src/pages/login/inputs.js
+++ admin/src/pages/login/inputs.js
@@ -5,6 +5,7 @@ const url = "http://localhost:3001/loginAdmin"
 const loginValidate = async (values) => {
     try{
         const res = await axios.post(url, values)
+        /* console.log(res) */
         return res.data.token
 
     }
