ea4e45aab44a13096b2cf12ec33e4483ea678827
cmacha2 <cristiancmg127@gmail.com>
2022-07-19 21:33:43 -0700

    add useLogin

--- client/AppWrapped.js
+++ client/AppWrapped.js
@@ -1,48 +1,10 @@
-import { useDispatch, useSelector } from "react-redux";
 import AuthStack from "./src/Wrappers/AuthStack";
 import AppStack from "./src/Wrappers/AppStack";
-import { ActivityIndicator, Alert, View } from "react-native";
-import { changeToken, changeUser, cleanErr } from "./src/redux/slices/signin";
-import { useEffect } from "react";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import { ActivityIndicator, View } from "react-native";
+import useLogin from "./src/screens/Main/customHooks/useLogin";
 
 export default function AppWrapped() {
-  let { token, err, isLoading } = useSelector((state) => state.signin);
-  const dispatch = useDispatch();
-
-  let getToken = async () => {
-    try {
-      let userToken = null;
-      let user = null;
-      userToken = await AsyncStorage.getItem("@token_id");
-      await AsyncStorage.getItem("@user").then((res) => {
-        if (res !== null) {
-          user = JSON.parse(res);
-          dispatch(changeUser(user));
-        }
-      });
-      dispatch(changeToken(userToken));
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    getToken();
-  }, [dispatch]);
-
-  if (err?.includes("incorrectos")) {
-    Alert.alert("Error", "Email o contraseña incorrectos.");
-    dispatch(cleanErr());
-  }
-  if (err?.includes("confirmado")) {
-    Alert.alert("Error", "Debes confirmar tu cuenta para ingresar.");
-    dispatch(cleanErr());
-  }
-  if (err === undefined) {
-    Alert.alert("Error", "El servidor se encuentra caído.");
-    dispatch(cleanErr());
-  }
+  const [token, isLoading] = useLogin()
 
   if (isLoading) {
     return (
---
+++ client/src/screens/Main/customHooks/useLogin.js
@@ -0,0 +1,47 @@
+import{ useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { changeToken, changeUser } from '../../../redux/slices/signin';
+
+const useLogin = () => {
+    let { token , err, isLoading } = useSelector((state) => state.signin);
+    const dispatch = useDispatch();
+  
+    let getToken = async () => {
+      try {
+        let userToken = null;
+        let user = null;
+        userToken = await AsyncStorage.getItem("@token_id");
+        await AsyncStorage.getItem("@user").then((res) => {
+          if (res !== null) {
+            user = JSON.parse(res);
+            dispatch(changeUser(user));
+          }
+        });
+        dispatch(changeToken(userToken));
+      } catch (error) {
+        console.log(error);
+      }
+    };
+  
+    useEffect(()=>{
+           getToken();
+        },[dispatch]);
+  
+    if (err?.includes("incorrectos")) {
+      Alert.alert("Error", "Email o contraseña incorrectos.");
+      dispatch(cleanErr());
+    }
+    if (err?.includes("confirmado")) {
+      Alert.alert("Error", "Debes confirmar tu cuenta para ingresar.");
+      dispatch(cleanErr());
+    }
+    if (err === undefined) {
+      Alert.alert("Error", "El servidor se encuentra caído.");
+      dispatch(cleanErr());
+    }
+
+    return [token, isLoading];
+}
+
+export default useLogin
