3c606d527bcbf03f9bfc953e9021fcfbc779dec9
cmacha2 <cristiancmg127@gmail.com>
2022-07-19 19:59:12 -0700

    add useShoppingCart

--- client/src/screens/Main/Cart.js
+++ client/src/screens/Main/Cart.js
@@ -1,95 +1,68 @@
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import React, { useEffect, useRef, useState } from "react";
-import { View, Text, Image, StatusBar, TouchableNativeFeedback, Modal, TouchableOpacity, Pressable } from "react-native";
-import { useSelector, useDispatch } from 'react-redux'
-import { AntDesign } from '@expo/vector-icons';
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StatusBar,
+  TouchableNativeFeedback,
+  Modal,
+} from "react-native";
 import CardDefault from "./modules/CardDefault";
 import CardNav from "./modules/CardNav";
 import CardProducts from "./modules/CardProducts";
 import styles from "./Styles/Cart.jsx";
 import CartModalBotton from "./modules/CartModalBotton";
 import CartModalTop from "./modules/CartModalTop";
+import useShoppingCart from "./customHooks/useShoppingCart";
 
 export default function EmptyCart() {
-
-  const { newItems } = useSelector((state) => state.products);
-  const [productsCart, setProductsCart] = useState([]);
   const [modal, setModal] = useState(false);
-  const [totalPrice, setTotalPrice] = useState(0);
-
-  /* console.log(animation) */
-  useEffect(() => {
-    AsyncStorage.getItem("@shoppingCart").then(res => {
-      if (res !== null) {
-        let products = JSON.parse(res);
-        setProductsCart(products);
-        let total = products.reduce((acc, cur) => {
-          return acc + Number(cur.price);
-        }, 0)
-        setTotalPrice(total.toFixed(2));
-      }
-    }).catch(err => {
-      console.log(err);
-    });
-    if (newItems === null) {
-      AsyncStorage.setItem("@shoppingCart", JSON.stringify([]))
-        .then(() => {
-          setProductsCart([]);
-        })
-    }
-  }, [newItems]);
-
+  const { productsCart, totalPrice } = useShoppingCart();
 
   return (
     <View style={styles.wrapper}>
       <StatusBar />
       <View style={styles.container}>
-
         <CardNav productsCart={productsCart} />
 
         <View style={styles.containerMain}>
-          {
-            productsCart.length === 0 ?
-              <CardDefault />
-              :
-              <>
-                <CardProducts productsCart={productsCart} />
+          {productsCart.length === 0 ? (
+            <CardDefault />
+          ) : (
+            <>
+              <CardProducts productsCart={productsCart} />
 
+              <TouchableNativeFeedback
+                onPress={() => {
+                  setModal(true);
+                }}
+              >
+                <View style={styles.arrowAnimated}>
+                  <Text style={styles.title}>¡COMPRAR YA!</Text>
+                </View>
+              </TouchableNativeFeedback>
 
-
-                <TouchableNativeFeedback
-                  onPress={() => {
-                    setModal(true)
-                  }}>
-                  <View style={styles.arrowAnimated}>
-                    <Text style={styles.title}>¡COMPRAR YA!</Text>
-                  </View>
-                </TouchableNativeFeedback>
-
-
-
-                <Modal visible={modal} transparent animationType={'slide'}>
-                  
-                    <View style={styles.modalPrueba}>
-                      <View style={styles.modalContainer}>
-                        <View style={styles.modalContent}>
-                          <CartModalTop setModal={setModal} totalPrice={totalPrice} />
-                          <CartModalBotton modal={modal} setModal={setModal} />
-                        </View>
-                      </View>
+              <Modal visible={modal} transparent animationType={"slide"}>
+                <View style={styles.modalPrueba}>
+                  <View style={styles.modalContainer}>
+                    <View style={styles.modalContent}>
+                      <CartModalTop
+                        setModal={setModal}
+                        totalPrice={totalPrice}
+                      />
+                      <CartModalBotton modal={modal} setModal={setModal} />
                     </View>
-                  {/* <View style={styles.modalContainer}>
+                  </View>
+                </View>
+                {/* <View style={styles.modalContainer}>
                     <View style={styles.modalContent}>
                       
                       <CartModalTop setModal={setModal} totalPrice={totalPrice}/>
                       <CartModalBotton modal={modal} setModal={setModal}/>
                     </View>
                   </View> */}
-                </Modal>
-
-              </>
-          }
-
+              </Modal>
+            </>
+          )}
         </View>
       </View>
     </View>
