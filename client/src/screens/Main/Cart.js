cd6c19c0e7cc88acfd8935edff76766780b345b7
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-07-21 18:44:29 -0300

    alert/cart no funciona el pago

--- client/src/screens/Main/Cart.js
+++ client/src/screens/Main/Cart.js
@@ -13,9 +13,12 @@ import styles from "./Styles/Cart.jsx";
 import CartModalBotton from "./modules/CartModalBotton";
 import CartModalTop from "./modules/CartModalTop";
 import useShoppingCart from "../../customHooks/useShoppingCart";
+import CustomAlertPayment from "../../components/CustomAlertPayment";
 
 export default function EmptyCart() {
+  const [alert, setAlert] = useState(false);
   const [modal, setModal] = useState(false);
+  const [flag, setFlag] = useState(1);
   const { productsCart, totalPrice } = useShoppingCart();
 
   return (
@@ -49,7 +52,14 @@ export default function EmptyCart() {
                         setModal={setModal}
                         totalPrice={totalPrice}
                       />
-                      <CartModalBotton modal={modal} setModal={setModal} />
+                      <CartModalBotton
+                        modal={modal}
+                        setModal={setModal}
+                        alert={alert}
+                        setAlert={setAlert}
+                        flag={flag}
+                        setFlag={setFlag}
+                        />
                     </View>
                   </View>
                 </View>
@@ -61,6 +71,21 @@ export default function EmptyCart() {
                     </View>
                   </View> */}
               </Modal>
+              <CustomAlertPayment
+                flag={flag}
+                setFlag={setFlag}
+                alert={alert}
+                setAlert={setAlert}
+                title={
+                  flag? "¡Su pago a sido exitoso!"
+                    : "¡Su pago a sido denegado!"
+                }
+                message={
+                  flag ?"Gracias por su compra" : "Por favor revise sus datos"
+                }
+                color={flag? "#01A601" : "#F70000"}
+                iconName={flag? "check-decagram" : "alert-circle"}
+              />
             </>
           )}
         </View>
