dddc61d4b75120a7f0bf32c61a056a849553ff8c
Facundo Ortiz <facundoagustinortiz@gmail.com>
2022-07-21 12:25:46 -0300

    fixed OTRO ERROR DE MACHADO

--- client/src/customHooks/useLogin.js
+++ client/src/customHooks/useLogin.js
@@ -1,48 +1,48 @@
-import{ useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { changeToken, changeUser, cleanErr } from '../../../redux/slices/signin';
-import { Alert } from 'react-native';
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { changeToken, changeUser, cleanErr } from "../redux/slices/signin";
+import { Alert } from "react-native";
 
 const useLogin = () => {
-    let { token , err, isLoading } = useSelector((state) => state.signin);
-    const dispatch = useDispatch();
-  
-    let getToken = async () => {
-      try {
-        let userToken = null;
-        let user = null;
-        userToken = await AsyncStorage.getItem("@token_id");
-        await AsyncStorage.getItem("@user").then((res) => {
-          if (res !== null) {
-            user = JSON.parse(res);
-            dispatch(changeUser(user));
-          }
-        });
-        dispatch(changeToken(userToken));
-      } catch (error) {
-        console.log(error);
-      }
-    };
-  
-    useEffect(()=>{
-           getToken();
-        },[dispatch]);
-  
-    if (err?.includes("incorrectos")) {
-      Alert.alert("Error", "Email o contraseña incorrectos.");
-      dispatch(cleanErr());
-    }
-    if (err?.includes("confirmado")) {
-      Alert.alert("Error", "Debes confirmar tu cuenta para ingresar.");
-      dispatch(cleanErr());
-    }
-    if (err === undefined) {
-      Alert.alert("Error", "El servidor se encuentra caído.");
-      dispatch(cleanErr());
-    }
+  let { token, err, isLoading } = useSelector((state) => state.signin);
+  const dispatch = useDispatch();
 
-    return [token, isLoading];
-}
+  let getToken = async () => {
+    try {
+      let userToken = null;
+      let user = null;
+      userToken = await AsyncStorage.getItem("@token_id");
+      await AsyncStorage.getItem("@user").then((res) => {
+        if (res !== null) {
+          user = JSON.parse(res);
+          dispatch(changeUser(user));
+        }
+      });
+      dispatch(changeToken(userToken));
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-export default useLogin
+  useEffect(() => {
+    getToken();
+  }, [dispatch]);
+
+  if (err?.includes("incorrectos")) {
+    Alert.alert("Error", "Email o contraseña incorrectos.");
+    dispatch(cleanErr());
+  }
+  if (err?.includes("confirmado")) {
+    Alert.alert("Error", "Debes confirmar tu cuenta para ingresar.");
+    dispatch(cleanErr());
+  }
+  if (err === undefined) {
+    Alert.alert("Error", "El servidor se encuentra caído.");
+    dispatch(cleanErr());
+  }
+
+  return [token, isLoading];
+};
+
+export default useLogin;
