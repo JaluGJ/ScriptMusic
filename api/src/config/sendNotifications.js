3f3ac207bca4cf2c4a5e2a770e3e651165c522a1
JaluGJ <gabriel.i.jalil@gmail.com>
2022-07-23 20:58:51 -0300

    Update sendNotifications.js

--- api/src/config/sendNotifications.js
+++ api/src/config/sendNotifications.js
@@ -1,27 +1,33 @@
-const User = require('../models/user/userSchema.js')
-const fetch = require('node-fetch')
+const User = require("../models/user/userSchema.js");
+const fetch = require("node-fetch");
 
-
-async function sendMultiplePushNotifications({ body }) {
-
-    const users = await User.find({})
-    const tokens = users.filter(user => user.pushToken).map(c=> c.pushToken)
-    //const sendUser = users.filter(user => user.pushToken)
-
-    await fetch('https://exp.host/--/api/v2/push/send', {
-      method: 'POST',
-      headers: {
-        host: 'exp.host',
-        accept: 'application/json',
-        'accept-encoding': 'gzip, deflate',
-        'content-type': 'application/json',
-      },
-      body: JSON.stringify({
-        to: tokens, // <-- replace with a list of Expo push tokens
-        title: 'Mira nuestra nueva oferta!',
-        body: `Tenemos una nueva promo disponible: ${body}`,
-      }),
-    });
+async function sendMultiplePushNotifications(body) {
+  try {
+    const users = await User.find({});
+    users
+      .filter((user) => user.pushToken)
+      .map(async (user) => {
+        try {
+          await fetch("https://exp.host/--/api/v2/push/send", {
+            method: "POST",
+            headers: {
+              accept: "application/json",
+              "accept-encoding": "gzip, deflate",
+              "content-type": "application/json",
+            },
+            body: JSON.stringify({
+              to: user.pushToken, // <-- replace with a list of Expo push tokens
+              title: "Atencion estimados usuarios!",
+              body: `Tenemos una nueva promo disponible: ${body}`,
+            }),
+          });
+        } catch (err) {
+          console.log(err);
+        }
+      });
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-module.exports = sendMultiplePushNotifications
+module.exports = sendMultiplePushNotifications;
