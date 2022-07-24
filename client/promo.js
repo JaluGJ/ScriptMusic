3e745de451c97b6c6ef1099a251c235b96e55dc3
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-07-24 00:30:56 -0300

    hardcode promos

---
+++ client/promo.js
@@ -0,0 +1,218 @@
+export let promos = [
+    {
+        "promoName":"2X1 Guitarras Clasicas",
+        "price":10,
+        "stock":10,
+        "description":"Solo en primavera productos a mitad de precioso.",
+        "image":"https://i.postimg.cc/x1ZjgtbQ/Guitarra-Clasicas.png",
+        "promo":"Descuento",
+        "items":[
+            {
+                "model": "Clásica Natural",
+                "brand": "Admira",
+                "price": 95.99,
+                "type": " Clasica",
+                "category": "Guitarra",
+                "stock": 10,
+                "image": "https://res.cloudinary.com/dzonjuriq/image/upload/v1657387337/script_music_img/alba_natural_natural-removebg-preview_pmhyla.png",
+                "description": "Si quieres aprender a tocar y no sabes qué modelo comprar, la guitarra Admira Sara probablemente sea una de las mejores opciones. Es un modelo ideal para principiantes y de los más vendidos de la marca, a diferencia de la Fiesta, esta tiene la tapa en Pino.",
+                "user": [],
+                "ratYCom": [],
+                "id": "62d4d6dcda220153870d76ad"
+            },
+        ]
+    },
+    {
+        "promoName":"Ofertas de Verano",
+        "price":10,
+        "stock":10,
+        "description":"Se ese amigo que lleva un Ukelele o una guitarra con nuestros descuentos!",
+        "image":"https://i.postimg.cc/QMLp4wBM/Negro-y-Celeste-Cl-sico-Negro-y-Ne-n-Electr-nica-y-Electrodom-sticos-Banner.png",
+        "promo":"Descuento",
+        "items":[
+            {
+                "ratYCom": [],
+                "model": "130B Bandurria",
+                "brand": "PRUDENCIO SÁEZ",
+                "price": 890,
+                "type": "Acustica",
+                "category": "Ukelele",
+                "stock": 31,
+                "image": "https://res.cloudinary.com/dzonjuriq/image/upload/v1658006798/script_music_img/Prudencio_S%C3%A1ez_130B_Bandurria_vet0bx.png",
+                "description": "Bandurria Prudencio Sáez 130B, AROS Y FONDO: Sapelly, DIAPASÓN: Ébano, toda maciza, MANGO: Cedro, PUENTE: Palosanto, CLAVIJERO: Dorado, TAPA: Abeto, ACABADO: Boca ovalada.",
+                "user": [],
+                "id": "62d4db39da220153870d7805"
+            },
+            {
+                "ratYCom": [],
+                "model": "QUK-30T",
+                "brand": "OQAN",
+                "price": 100,
+                "type": "Acustica",
+                "category": "Ukelele",
+                "stock": 27,
+                "image": "https://res.cloudinary.com/dzonjuriq/image/upload/v1657995912/script_music_img/OQAN_QUK-30T_fobz1j.png",
+                "description": "UKELELE TENOR QUK-30TUKELELE Ukelele tamaño tenor. Cuerpo en madera de sapeli. Mástil de nato con binding blanco. Diapasón de 'compound rosewood. Clavijeros Die-cast.",
+                "user": [],
+                "id": "62d4d73eda220153870d76cd"
+            },
+        ]
+    },
+    {
+        "promoName":"Saxofonos a 70%",
+        "price":10,
+        "stock":10,
+        "description":"El Jazz y sus instrumento estrella estan de descuento en ScriptMusic!",
+        "image":"https://i.postimg.cc/8cwfCMXC/Saxofones.png",
+        "promo":"Descuento",
+        "items":[
+            {
+                "ratYCom": [],
+                "model": "AS-220 Saxofón Alto",
+                "brand": "BRESSANT",
+                "price": 559.99,
+                "type": "Saxofon",
+                "category": "Viento",
+                "stock": 21,
+                "image": "https://res.cloudinary.com/dzonjuriq/image/upload/v1657998782/script_music_img/Bressant_AS-220_Saxof%C3%B3n_Alto_iusuyz.png",
+                "description": "Los Saxofones están diseñados para estudiantes que inician sus estudios musicales. Dada la fiabibilidad que ofrece Bressant hacen que este instrumento sea recomendado por numerosos profesionales a sus alumnos. Fabricado con materiales de alta calidad. Gama estudio. Afinación 442Hz. Apoyapulgar de metal. Llaves FA frontal. Muelles de acero. Lacado oro. Campana grabada. Estuche OMEGAIGE. Accesorios incluidos: Boquilla Gamuza.",
+                "user": [],
+                "id": "62d4dc03da220153870d783a"
+            },
+            {
+                "ratYCom": [],
+                "model": "JSS1100Q SAXO SOPRANO",
+                "brand": "JUPITER",
+                "price": 2349,
+                "type": "Saxofon",
+                "category": "Viento",
+                "stock": 30,
+                "image": "https://res.cloudinary.com/dzonjuriq/image/upload/v1657998806/script_music_img/JUPITER_JSS1100Q_SAXO_SOPRANO_zwnq7r.png",
+                "description": "JSS1100Q. SAXOFÓN SOPRANO JÚPITER, lacado. Dos tudeles, recto y curvo, hechos a mano de una sola pieza. Cuerpo lacado oro. Llave de sol agudo.Mecanismo en cobre forjado.Soporte apoyapulgar regulable para una mejor adaptación en función de la morfologia. Si/Do# articulados. Zapatillas de alta gama en cuero PISONI.",
+                "user": [],
+                "id": "62d4dc0fda220153870d783c"
+            },
+            {
+                "ratYCom": [],
+                "model": "Saxofón Alto YAS 480S",
+                "brand": "YAMAHA",
+                "price": 1979,
+                "type": "Saxofon",
+                "category": "Viento",
+                "stock": 25,
+                "image": "https://res.cloudinary.com/dzonjuriq/image/upload/v1657998834/script_music_img/Saxof%C3%B3n_Alto_Yamaha_YAS_480S_nf9m7t.png",
+                "description": "Saxofón alto. Acabado: Dorado. Los saxofones YAS/YTS-480 son un paso más allá. Con un poco más de resistencia, tienen un sonido autoritario, además de una gran flexibilidad, con sus defensas independientes. Ergonomía, tocabilidad y afinación excelentes, tal como se espera de un instrumento Yamaha.",
+                "user": [],
+                "id": "62d4dc18da220153870d7840"
+            },
+            {
+                "ratYCom": [],
+                "model": "Saxofón Alto YAS-62S 04",
+                "brand": "YAMAHA",
+                "price": 2549,
+                "type": "Saxofon",
+                "category": "Viento",
+                "stock": 40,
+                "image": "https://res.cloudinary.com/dzonjuriq/image/upload/v1657998966/script_music_img/Saxof%C3%B3n_Alto_Yamaha_YAS-62S_04_jcbffb.png",
+                "description": "Saxofón alto de la serie Professional deYamaha. Modelo YAS 62S04 nueva versión con acabado de baño de plata. Yamaha YAS 62SEl cuello G1 mejora la proyección tonal y libera el registro superior. La tecla de octava ofrece una acción más rápida y suave. El diseño del receptor del cuello está basado en el de la serie Custom para aumentar el nivel de resonancia.",
+                "user": [],
+                "id": "62d4dc20da220153870d7842"
+            },
+        ]
+    },
+    {
+        "promoName":"Promo Escolar",
+        "price":10,
+        "stock":10,
+        "description":"El inicio de clases es costoso. En ScriptMusic sabemos eso y quisimos ayudar a los padres con nuestras rebajas en flautas.",
+        "image":"https://i.postimg.cc/ZK01jZP9/Escolar.png",
+        "promo":"Descuento",
+        "items":[
+            {
+                "ratYCom": [],
+                "model": "TFP-239 Flautín",
+                "brand": "PEARL FLAUTAS",
+                "price": 366,
+                "type": "Esfuerzo",
+                "category": "Viento",
+                "stock": 24,
+                "image": "https://res.cloudinary.com/dzonjuriq/image/upload/v1658001635/script_music_img/FLAUTAS_PEARL_TFP-239_Flaut%C3%ADn_kdj0yj.png",
+                "description": "Flautín Pearl TFP-239-239.",
+                "user": [],
+                "id": "62d4d9fcda220153870d77b6"
+            },
+            {
+                "ratYCom": [],
+                "model": "OBOE 091/3 ART TF",
+                "brand": "BULGHERONI",
+                "price": 2885,
+                "type": "Flauta ",
+                "category": "Viento",
+                "stock": 29,
+                "image": "https://res.cloudinary.com/dzonjuriq/image/upload/v1657998618/script_music_img/OBOE_BULGHERONI_0913_ART_TF_sxpuwm.png",
+                "description": "LLAVES PLATEADAS-LLAVE DE SIb-DOBLE LLAVE DE FA-3ª LLAVE DE OCTAVA-PLATO ABIERTO OIDO NOTA RE-DO SOSTENIDO ARTICULADO.",
+                "user": [],
+                "id": "62d4dbe2da220153870d7832"
+            },
+            {
+                "ratYCom": [],
+                "model": "OBOE YOB-432MF",
+                "brand": "YAMAHA",
+                "price": 4129,
+                "type": "Flauta ",
+                "category": "Viento",
+                "stock": 58,
+                "image": "https://res.cloudinary.com/dzonjuriq/image/upload/v1657998536/script_music_img/OBOE_YAMAHA_YOB-432MF_cu7ine.png",
+                "description": "Como YOB-432M con llave de Fa de resonancia.",
+                "user": [],
+                "id": "62d4dbeeda220153870d7834"
+            },
+        ]
+    },
+    {
+        "promoName":"Teclados a nada de precio",
+        "price":10,
+        "stock":10,
+        "description":"Teclados, solo un teclado",
+        "image":"https://i.postimg.cc/mrVw01BT/Teclado.png",
+        "promo":"Descuento",
+        "items":[
+            {
+                "ratYCom": [],
+                "model": "Roland FP-30X Negro digital",
+                "brand": "ROLAND",
+                "price": 699,
+                "type": "Digital",
+                "category": "Teclado",
+                "stock": 37,
+                "image": "https://res.cloudinary.com/dzonjuriq/image/upload/v1657994007/script_music_img/Roland_FP-30X_Negro_digital_qwegih.png",
+                "description": "Combinando diseños en movimiento con una jugabilidad suprema y características de peso pesado, la serie FP-X representa una nueva generación de pianos portátiles de Roland. Descubra cuatro modelos para cada presupuesto, desde el FP-10 de nivel de entrada y su conjunto de características sobresalientes hasta el modelo insignia FP-90X con el mejor modelado de piano acústico puro de su clase. Donde quiera que desee llevar su música, la serie FP-X lo llevará allí.",
+                "user": [],
+                "id": "62d4da57da220153870d77c7"
+            },
+        ]
+    },
+    {
+        "promoName":"Guitarra acustica",
+        "price":10,
+        "stock":10,
+        "description":"NADANDANDADANDNADNANANDA",
+        "image":"https://i.postimg.cc/hvm3W42M/Acustico.png",
+        "promo":"Descuento",
+        "items":[
+            {
+                "ratYCom": [],
+                "model": "35F AMBAR Flamenco LT",
+                "brand": "PRUDENCIO SÁEZ",
+                "price": 735,
+                "type": "Acustica",
+                "category": "Guitarra",
+                "stock": 36,
+                "image": "https://res.cloudinary.com/dzonjuriq/image/upload/v1657994948/script_music_img/Prudencio_S%C3%A1ez_35F_AMBAR_Flamenco_Edici%C3%B3n_Especial_LT_con_Funda_ztzzb7.png",
+                "description": "Nueva guitarra Flamenca Modelo35F en acabado Amarillo Edición Especial. Trás el gran éxito que ha tenido su hermana en color Naranja, Prudencio Saez nos envía la guitarra en un acabado amarillo especial.",
+                "user": [],
+                "id": "62d4dcc5da220153870d785c"
+            },
+        ]
+    },
+]
---
+++ client/src/screens/Main/PromoDetail.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const PromoDetail = () => {
+    
+  return (
+    <View>
+
+    </View>
+  )
+}
+
+export default PromoDetail
