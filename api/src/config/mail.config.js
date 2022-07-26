c8b2d5c383cbfc7352938dbf8d3ec314c61d5ef1
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-07-26 00:08:27 -0300

    prueba2

--- api/src/config/mail.config.js
+++ api/src/config/mail.config.js
@@ -46,28 +46,28 @@ const sendEmail = async (email, subject, html) => {
 const getTemplate = (name, token) => {
   return `
   <div id="email___content"
-  style="font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif; width: 350px;align-items: center;display: flex;flex-direction: column;background-color: rgb(238, 238, 238);padding: 5px;">
-  <div style="width:100%;align-items: center; justify-content: center; display: flex;flex-direction: column;">
+        style="font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif; width: 350px;background-color: rgb(202, 202, 202);padding: 5px;">
+        <div style="width:100%;">
 
-      <img style="width:150px;padding: 5px;" src="https://i.postimg.cc/pTcwbcgr/Sm-Logo02-PNG.png" alt=""><br />
-      <p style="color: #000000;font-size: 25px; font-weight: bold">¡Bienvenido!</p>
-  </div>
-  <div
-  style="width:98%;align-items: center; justify-content: center; display: flex;flex-direction: column;background-color: white;border-radius: 10px;padding: 4px;">
-  <p style="color: #000000;font-size: 20px; font-weight: bold">Hola, ${name}</p>
-  <p style="color: #000000;font-size: 15px; text-align: center;">Estamos muy contentos por tenerte con nosotros. Para continuar,
-          necesitamos que confirmes tu cuenta, solo presiona el siguiente botón:</p>
+            <img style="width:150px;padding: 5px;" src="https://i.postimg.cc/pTcwbcgr/Sm-Logo02-PNG.png" alt=""><br />
+            <p style="color: #000000;font-size: 25px; font-weight: bold">¡Bienvenido!</p>
+        </div>
+        <div
+        style="width:98%;background-color: white;border-radius: 10px;padding: 4px;">
+        <p style="color: #000000;font-size: 20px; font-weight: bold">Hola, ${name}</p>
+        <p style="color: #000000;font-size: 15px; text-align: center;">Estamos muy contentos por tenerte con nosotros. Para continuar,
+                necesitamos que confirmes tu cuenta, solo presiona el siguiente botón:</p>
 
-      <p style="color: #000000;font-size: 15px;text-align: center;">Si tienes problemas o alguna pregunta, responde este mail,
-          siempre
-          estaremos felices de poder ayudarte.</p>
-      <a style="color: #000000; font-size: 15px; text-decoration: underline;"
-          href="https://sm.up.railway.app/user/confirm/token/${token}" target="_blank"><button
-              style="background-color: #000000; border-radius: 5px; width: 150px; height: 30px; border: 1px solid; cursor: pointer;color: white;">Confirmar cuenta</button></a><br />
-      <p style="color: #000000;font-size: 15px;">Saludos, el equipo de ScriptMusic.</p>
+            <p style="color: #000000;font-size: 15px;text-align: center;">Si tienes problemas o alguna pregunta, responde este mail,
+                siempre
+                estaremos felices de poder ayudarte.</p>
+            <a style="color: #000000; font-size: 15px; text-decoration: underline;"
+                href="https://sm.up.railway.app/user/confirm/token/${token}" target="_blank"><button
+                    style="background-color: #000000; border-radius: 5px; width: 150px; height: 30px; border: 1px solid; cursor: pointer;color: white;">Confirmar cuenta</button></a><br />
+            <p style="color: #000000;font-size: 15px;">Saludos, el equipo de ScriptMusic.</p>
 
-  </div>
-</div>
+        </div>
+    </div>
       `;
 };
 
