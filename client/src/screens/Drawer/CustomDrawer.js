1cf7b68a9f28817ae707b13da56a1473199e8c03
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-07-26 17:34:50 -0300

    styles/nivel

--- client/src/screens/Drawer/CustomDrawer.js
+++ client/src/screens/Drawer/CustomDrawer.js
@@ -22,7 +22,6 @@ const CustomDrawer = (props) => {
   const { user } = useSelector((state) => state.signin);
   const dispatch = useDispatch();
   var progress = bought.length > 0 ?  bought.length / 20 : 0;
-  var status = bought.length > 0 && bought.length <= 2  ? "Level 1" : bought.length > 2 && bought.length <= 6 ? "Level 2" : bought.length > 6 && bought.length <= 10 ? "Level 3" : bought.length > 10 && bought.length <= 15 ? "Level 4" : bought.length > 15 && bought.length <= 20 ? "Level 5" : "Level 0";
 
   return (
     <View style={{ flex: 1 }}>
@@ -32,22 +31,19 @@ const CustomDrawer = (props) => {
           source={require("../../../assets/menu.png")}
           style={styles.imgBackground}
         >
+          <View style={styles.borderUser}>
+
           <Image
             source={user ? {uri: user.image} : require("../../../assets/user.png")}
             style={styles.userImage}
           />
           <View style={styles.progressCircle}>    
-           <Progress.Circle size={85} showsText={false} thickness={3} progress={progress} />
+           <Progress.Circle size={90} showsText={false} thickness={3} progress={progress} borderColor="#000000" color='orange' borderWidth={2}/>
+          </View>
           </View>
           <Text style={styles.userName}>
             {user ? user.firstName + " " + user.lastName : "Cargando..."}
           </Text>
-          <Text style={styles.level}>
-         {status}
-         </Text>
-         <Text style={styles.levelSubtitle}>
-            ScriptMusic Points
-         </Text>
         </ImageBackground>
 
         <View style={styles.containerItems}>
--- client/src/screens/Main/helpers/HomeStylePromos.js
+++ client/src/screens/Main/helpers/HomeStylePromos.js
@@ -10,6 +10,7 @@ export let StylesPromos = [
         img: promo2,
         containerInfo: styles.containerInfo,
         containerModel: styles.containerModel,
+        containerImage: styles.containerImageOne,
         containerText: styles.containerText,
         containerTypePromo:styles.containerTypePromoOne
     },
