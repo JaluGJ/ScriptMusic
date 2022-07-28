fab7fcea50d0a78a1874d54120619b9b7297963d
JaluGJ <gabriel.i.jalil@gmail.com>
2022-07-28 04:47:33 -0300

    Update Loading.jsx

--- admin/src/components/Loading/Loading.jsx
+++ admin/src/components/Loading/Loading.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-//import './Loading.scss';
+
 
 function Loading() {
     const image = 'https://media0.giphy.com/media/rypIwxQ2VDTzifwhWD/giphy.gif?cid=790b76118f9d66ad10ba297ada43bb984b6d34e687ad5de4&rid=giphy.gif&ct=g'
